505e1d71c9ee1f56c09a44da81b8a74eccd3a614
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2025-06-13 22:35:45 +0800

    exppattern

--- src/blockly/generators/index.js
+++ src/blockly/generators/index.js
@@ -3,6 +3,7 @@ import gen_gameevent_hp from './gameevent_hp';
 import gen_gameevent_judge from './gameevent_judge';
 import gen_player from './player';
 import gen_card from './card';
+import gen_matcher from './matcher';
 
 export default () => {
   gen_basic();
@@ -10,4 +11,5 @@ export default () => {
   gen_gameevent_judge();
   gen_player();
   gen_card();
+  gen_matcher();
 };
