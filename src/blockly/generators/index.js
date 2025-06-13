e2db113af155741574ea07610bee4865f0125f93
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2025-06-13 19:37:05 +0800

    卡牌相关

--- src/blockly/generators/index.js
+++ src/blockly/generators/index.js
@@ -1,11 +1,13 @@
 import gen_basic from './basic';
 import gen_gameevent_hp from './gameevent_hp';
 import gen_gameevent_judge from './gameevent_judge';
-import gen_player from './player'
+import gen_player from './player';
+import gen_card from './card';
 
 export default () => {
   gen_basic();
   gen_gameevent_hp();
-  gen_player();
   gen_gameevent_judge();
+  gen_player();
+  gen_card();
 };
