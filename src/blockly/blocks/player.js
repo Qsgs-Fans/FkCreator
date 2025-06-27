0c11bd48729d8ab5825b045321311653555b3977
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2025-06-28 05:41:21 +0800

    阶段性搬运之Player

--- src/blockly/blocks/player.js
+++ src/blockly/blocks/player.js
@@ -134,7 +134,101 @@ Blockly.Blocks['player_property_read_by_fn'] = {
   }
 };
 
+Blockly.Blocks['player_get_card_ids'] = {
+  init: function () {
+    this.appendValueInput('PLAYER').setCheck('Player').appendField('角色');
+
+    this.appendDummyInput('PROPERTY_INPUT')
+      .appendField('的')
+      .appendField(
+        new Blockly.FieldDropdown(
+          [
+            ['手牌区', 'h'],
+            ['装备区', 'e'],
+            ['判定区', 'j'],
+
+            ['手牌区+装备区', 'he'],
+            ['手牌区+判定区', 'hj'],
+            ['场上', 'ej'],
+            ['区域', 'hej'],
+          ],
+        ),
+        'FLAG'
+      )
+      .appendField('内所有牌');
+
+    this.setColour(230);
+    this.setTooltip('获取角色某区域(或多个区域)内的所有牌');
+    this.setHelpUrl('');
+
+    this.setOutput(true, 'Array');
+  },
+};
+
+Blockly.Blocks['player_used_times'] = {
+  init: function () {
+    this.appendValueInput('PLAYER').setCheck('Player').appendField('角色');
+
+    this.appendDummyInput('HISTORY_INPUT')
+      .appendField('在')
+      .appendField(
+        new Blockly.FieldDropdown(
+          [
+            ['本阶段', 'HistoryPhase'],
+            ['本回合', 'HistoryTurn'],
+            ['本轮', 'HistoryRound'],
+            ['本局游戏', 'HistoryGame'],
+          ],
+        ),
+        'HISTORY'
+      )
+      .appendField('内使用')
+
+    this.appendValueInput('NAME').setCheck('String')
+      .appendField(
+      new Blockly.FieldDropdown(
+        [
+          ['牌名', 'Card'],
+          ['技能', 'Skill'],
+        ],
+      ),
+      'TYPE');
+    this.appendDummyInput()
+      .appendField('的次数');
+
+    this.setColour(230);
+    this.setTooltip('获得角色在某时机使用了多少次某个牌名或技能');
+    this.setHelpUrl('');
+
+    this.setOutput(true, 'Number');
+  },
+};
+
 export default [
+  {
+    type: 'player_has_skill',
+    message0: '角色 %1 是否拥有技能 %2',
+    args0: [
+      { type: 'input_value', name: 'PLAYER', check: 'Player' },
+      { type: 'input_value', name: 'NAME', check: 'String', align: 'RIGHT' }
+    ],
+    colour: 230,
+    tooltip: '获得角色是否拥有某个技能',
+    output: 'Boolean',
+    inputsInline: true,
+  },
+  {
+    type: 'player_has_delay',
+    message0: '角色 %1 的判定区内是否有延时锦囊 %2',
+    args0: [
+      { type: 'input_value', name: 'PLAYER', check: 'Player' },
+      { type: 'input_value', name: 'DELAY', check: 'String', align: 'RIGHT' }
+    ],
+    colour: 230,
+    tooltip: '获得角色的判定区内是否有特定名字的延时锦囊',
+    output: 'Boolean',
+    inputsInline: true,
+  },
   {
     type: 'player_get_mark',
     message0: '角色 %1 的标记 %2',
@@ -147,11 +241,42 @@ export default [
     output: null,
     inputsInline: true,
   },
-  // getCardIds
-  // hasDelayedTrick
-  // getPile
-  // distanceTo
-  // inMyAttackRange
+  {
+    type: 'player_get_pile',
+    message0: '角色 %1 的私人牌堆 %2',
+    args0: [
+      { type: 'input_value', name: 'PLAYER', check: 'Player' },
+      { type: 'input_value', name: 'PILE', check: 'String', align: 'RIGHT' }
+    ],
+    colour: 230,
+    tooltip: '获得角色的私人牌堆',
+    output: 'Array',
+    inputsInline: true,
+  },
+  {
+    type: 'player_distance_to',
+    message0: '角色 %1 ==计算=> 角色 %2 的距离',
+    args0: [
+      { type: 'input_value', name: 'FROM', check: 'Player' },
+      { type: 'input_value', name: 'TO', check: 'Player', align: 'RIGHT' }
+    ],
+    colour: 230,
+    tooltip: '获得角色单向计算另一个角色的距离（如果其中一方已死亡或被移出游戏，会返回-1）',
+    output: 'Number',
+    inputsInline: true,
+  },
+  {
+    type: 'player_in_attack_range',
+    message0: '角色 %1 的攻击范围内是否含有角色 %2',
+    args0: [
+      { type: 'input_value', name: 'FROM', check: 'Player' },
+      { type: 'input_value', name: 'TO', check: 'Player', align: 'RIGHT' }
+    ],
+    colour: 230,
+    tooltip: '获得角色的攻击范围内内是否有另一名角色',
+    output: 'Boolean',
+    inputsInline: true,
+  },
   // usedCardTimes
   // usedSkillTimes
   // hasSkill
--- src/blockly/toolbox_categories/player.js
+++ src/blockly/toolbox_categories/player.js
@@ -12,7 +12,55 @@ export default {
       kind: 'block'
     },
     {
+      type: 'player_get_card_ids',
+      kind: 'block'
+    },
+    {
+      type: 'player_used_times',
+      kind: 'block',
+      inputs: {
+        NAME: {
+          shadow: {
+            type: 'text',
+            fields: {
+              TEXT: ''
+            }
+          }
+        },
+      }
+    },
+    {
+      type: 'player_has_skill',
+      kind: 'block',
+      inputs: {
+        NAME: {
+          shadow: {
+            type: 'text',
+            fields: {
+              TEXT: ''
+            }
+          }
+        },
+      }
+    },
+    {
+      type: 'player_has_delay',
+      kind: 'block'
+    },
+    {
       type: 'player_get_mark',
+      kind: 'block'
+    },
+    {
+      type: 'player_get_pile',
+      kind: 'block'
+    },
+    {
+      type: 'player_distance_to',
+      kind: 'block'
+    },
+    {
+      type: 'player_in_attack_range',
       kind: 'block'
     },
   ]
