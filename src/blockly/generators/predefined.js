b503fde8175e28cf90c14c9f4f727e9005c7ac99
tomson <tomsonorange@163.com>
2025-09-27 21:33:41 +0000

    !3 补全了Cards.lua的所有预定义变量 * 补全了Cards.lua的所有预定义变量

--- src/blockly/generators/predefined.js
+++ src/blockly/generators/predefined.js
@@ -5,4 +5,26 @@ export default () => {
   luaGenerator.forBlock['predefined_card_suit_club'] = () => ['Card.Club', Order.ATOMIC];
   luaGenerator.forBlock['predefined_card_suit_heart'] = () => ['Card.Heart', Order.ATOMIC];
   luaGenerator.forBlock['predefined_card_suit_diamond'] = () => ['Card.Diamond', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_suit_nosuit'] = () => ['Card.NoSuit', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_color_black'] = () => ['Card.Black', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_color_red'] = () => ['Card.Red', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_type_basic'] = () => ['Card.TypeBasic', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_type_trick'] = () => ['Card.TypeTrick', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_type_equip'] = () => ['Card.TypeEquip', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_subtype_none'] = () => ['Card.SubtypeNone', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_subtype_delayedtrick'] = () => ['Card.SubtypeDelayedtrick', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_subtype_weapon'] = () => ['Card.SubtypeWeapon', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_subtype_armor'] = () => ['Card.SubtypeArmor', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_subtype_defensive_ride'] = () => ['Card.SubtypeDefensiveRide', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_subtype_offensive_ride'] = () => ['Card.SubtypeOffensiveRide', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_subtype_treasure'] = () => ['Card.SubtypeTreasure', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_unknown'] = () => ['Card.Unknown', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_player_hand'] = () => ['Card.PlayerHand', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_player_equip'] = () => ['Card.PlayerEquip', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_player_judge'] = () => ['Card.PlayerJudge', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_player_special'] = () => ['Card.PlayerSpecial', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_processing'] = () => ['Card.Processing', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_draw_pile'] = () => ['Card.DrawPile', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_discard_pile'] = () => ['Card.DiscardPile', Order.ATOMIC];
+  luaGenerator.forBlock['predefined_card_void'] = () => ['Card.Void', Order.ATOMIC];
 }
--- src/blockly/toolbox_categories/predefined.js
+++ src/blockly/toolbox_categories/predefined.js
@@ -19,5 +19,97 @@ export default {
       type: 'predefined_card_suit_diamond',
       kind: 'block'
     },
+    {
+      type: 'predefined_card_suit_nosuit',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_color_black',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_color_red',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_color_nocolor',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_type_basic',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_type_trick',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_type_equip',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_subtype_none',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_subtype_delayedtrick',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_subtype_weapon',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_subtype_armor',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_subtype_defensive_ride',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_subtype_offensive_ride',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_subtype_treasure',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_unknown',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_player_hand',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_player_equip',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_player_judge',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_player_special',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_processing',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_draw_pile',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_discard_pile',
+      kind: 'block'
+    },
+    {
+      type: 'predefined_card_void',
+      kind: 'block'
+    },
   ]
 };
